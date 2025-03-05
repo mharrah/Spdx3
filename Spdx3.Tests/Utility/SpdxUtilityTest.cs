4ab33fcf8bd836686c84353b1bdd06ef418d585b
Matthew Harrah <mharrah@paylocity.com>
2025-03-05 18:31:55 -0500

    Adding test

---
+++ Spdx3.Tests/Utility/SpdxUtilityTest.cs
@@ -0,0 +1,43 @@
+using Spdx3.Exceptions;
+using Spdx3.Model;
+using Spdx3.Model.Core.Classes;
+using Spdx3.Model.Extension.Classes;
+using Spdx3.Model.SimpleLicensing.Classes;
+using Spdx3.Utility;
+using File = Spdx3.Model.Software.Classes.File;
+
+namespace Spdx3.Tests.Utility;
+
+public class SpdxUtilityTest
+{
+    [Fact]
+    public void SpdxUtility_SpdxTypeForClass_ShouldReturnSpdxTypes()
+    {
+        Assert.Equal("Annotation",  SpdxUtility.SpdxTypeForClass(typeof(Annotation)));
+        Assert.Equal("simplelicensing_LicenseExpression",  SpdxUtility.SpdxTypeForClass(typeof(LicenseExpression)));
+        Assert.Equal("software_File",  SpdxUtility.SpdxTypeForClass(typeof(File)));
+        Assert.Equal("extension_CdxPropertiesExtension",  SpdxUtility.SpdxTypeForClass(typeof(CdxPropertiesExtension)));
+    }
+    
+    [Fact]
+    public void SpdxUtility_SpdxTypeForClass_ShouldThrowForNonSpdxTypes()
+    {
+        var ex = Record.Exception(() =>  SpdxUtility.SpdxTypeForClass(typeof(string)));
+
+        Assert.NotNull(ex);
+        Assert.IsType<Spdx3Exception>(ex);
+        Assert.Equal("Unable to determine SPDX3 node type value for System.String", ex.Message);
+    }
+    
+        
+    [Fact]
+    public void SpdxUtility_SpdxTypeForClass_ShouldThrowForTypeInEmptyNamespace()
+    {
+        var t = typeof(TestClassWithEmptyNamespace);
+        var ex = Record.Exception(() =>  SpdxUtility.SpdxTypeForClass(t));
+
+        Assert.NotNull(ex);
+        Assert.IsType<Spdx3Exception>(ex);
+        Assert.Equal("Unable to determine SPDX3 node type value for TestClassWithEmptyNamespace", ex.Message);
+    }
+}
